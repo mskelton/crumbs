c4701886447259c7544d693b4bf23be6c50e7082
Mark Skelton <mdskelton99@gmail.com>
2019-10-10 22:29:59 -0500

    Update eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,26 +1,21 @@
 module.exports = {
-  extends: ['standard', 'prettier', 'plugin:prettier/recommended'],
-  plugins: ['prettier'],
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
   env: {
     commonjs: true,
     es6: true,
     node: true,
   },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      parser: '@typescript-eslint/parser',
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
-      ],
-      rules: { '@typescript-eslint/explicit-function-return-type': 'off' },
-    },
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
   ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
     'sort-keys': 'error',
   },
 }
