90514449289a725760e968c49ee33157c8d8cb57
Mark Skelton <mdskelton99@gmail.com>
2019-10-11 06:03:34 -0500

    React linting

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -3,14 +3,26 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['standard', 'plugin:@typescript-eslint/recommended'],
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
   },
+  plugins: ['react-hooks'],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'comma-dangle': ['error', 'always-multiline'],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-sort-props': 'error',
     'sort-keys': 'error',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
