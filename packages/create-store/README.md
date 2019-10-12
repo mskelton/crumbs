39b157d7498852873d5221103ef1eee4e9d61800
Mark Skelton <mdskelton99@gmail.com>
2019-10-12 08:56:23 -0500

    Add create store package

---
+++ packages/create-store/README.md
@@ -0,0 +1,17 @@
+# @sage/create-store
+
+>
+
+## Description
+
+## Installation
+
+```sh
+# Yarn
+yarn add @sage/create-store
+
+# npm
+npm install @sage/create-store
+```
+
+## Usage
---
+++ packages/create-store/src/index.tsx
@@ -0,0 +1,60 @@
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useReducer,
+  Reducer,
+  Dispatch,
+} from 'react'
+
+interface ProviderProps {
+  children: ReactNode
+}
+
+export class InvalidReducerAction extends Error {
+  constructor(actionType: string) {
+    super(`Invalid action type: ${actionType}.`)
+
+    this.name = 'InvalidReducerAction'
+  }
+}
+
+export function createStore<State, Action>(
+  initialState: State,
+  displayName: string,
+  reducer: (state: State, action: Action) => State
+) {
+  const StateContext = createContext(initialState)
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const DispatchContext = createContext<Dispatch<Action>>(null!)
+
+  function Provider({ children }: ProviderProps) {
+    const [state, dispatch] = useReducer<Reducer<State, Action>>(
+      reducer,
+      initialState
+    )
+
+    return (
+      <StateContext.Provider value={state}>
+        <DispatchContext.Provider value={dispatch}>
+          {children}
+        </DispatchContext.Provider>
+      </StateContext.Provider>
+    )
+  }
+
+  Provider.displayName = displayName
+
+  function useHook(): [State, Dispatch<Action>] {
+    const state = useContext(StateContext)
+    const dispatch = useContext(DispatchContext)
+
+    return [state, dispatch]
+  }
+
+  useHook.useDispatchOnly = function useDispatchOnly() {
+    return useContext(DispatchContext)
+  }
+
+  return { Provider, useHook }
+}
